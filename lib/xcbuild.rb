3b988ef52b1aeae2f2c0ca418aeee60037c48664
Ashton Williams <AJWilliams@odecee.com.au>
2015-04-22 20:18:21 +1000

    more setup

---
+++ lib/xcbuild.rb
@@ -0,0 +1,5 @@
+require "xcbuild/version"
+
+module XCBuild
+  # Your code goes here...
+end
---
+++ spec/xcbuild_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe XCBuild do
+  it 'has a version number' do
+    expect(XCBuild::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
