f0a60887d4354da7e8cab2a307cea82bb73aac02
Ashton Williams <AJWilliams@odecee.com.au>
2015-04-23 10:39:58 +1000

    kinda works

---
+++ lib/xcbuild/options.rb
@@ -0,0 +1,46 @@
+require 'yaml'
+
+module XCBuild
+  class Options
+
+    attr_accessor :options, :project, :workspace, :target, :scheme
+
+    def initialize(args)
+      
+    end
+    
+    def self.yml_filename
+      '.xcbuild.yml'
+    end
+
+    def self.yml
+      @yml ||= File.exist?(yml_filename) ? YAML.load_file(yml_filename) : {}
+    end
+
+    def configure_from_yml
+      configure_project
+      configure_workspace
+      configure_target
+      configure_scheme
+    end
+    
+    def configure_project_from_yml
+      self.project ||= self.class.yml["project"] if self.class.yml["project"]
+      self.project ||= Dir.glob('.xcodeproj').first
+    end
+
+    def configure_workspace_from_yml
+      self.workspace ||= self.class.yml["workspace"] if self.class.yml["workspace"]
+      self.project ||= Dir.glob('.xcodeproj').first
+    end
+
+    def configure_target_from_yml
+      self.target ||= self.class.yml["target"] if self.class.yml["target"]
+    end
+
+    def configure_scheme_from_yml
+      self.scheme ||= self.class.yml["scheme"] if self.class.yml["scheme"]
+    end
+
+  end
+end
